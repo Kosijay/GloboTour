43f2d27eba1d8b4182aa016343a0dc6a238e9cdd
Kosisochukwu Eziaghighala <eziikay007@gmail.com>
2022-10-22 01:57:43 +0100

    Globochat Changed to Globotour. How i learn Navigating with fragments and recyclerview refreshing. files like fragment_jay_touch.xml were just there to refresh my memory

--- app/src/main/java/com/sriyank/globochat/MainActivity.kt
+++ app/src/main/java/com/sriyank/globochat/MainActivity.kt
@@ -2,34 +2,59 @@ package com.sriyank.globochat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var toolbar        : MaterialToolbar
+    private lateinit var navController  : NavController
+    private lateinit var navigationView: NavigationView
+    private lateinit var    drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get NavHost and NavController
+        // Initialize Views
+        toolbar         = findViewById(R.id.activity_main_toolbar)
+        navigationView  = findViewById(R.id.nav_view)
+        drawerLayout    = findViewById(R.id.my_drawer_layout)
+
+        // Get NavHostFragment and NavController
         val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
-        navController = navHostFrag.navController
+        navController   = navHostFrag.navController
 
-        // Get AppBarConfiguration
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        // Define AppBarConfiguration : Connect DrawerLayout with Navigation Component
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
-        // Link ActionBar with NavController
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        // Connect toolbar with NavController
+        toolbar.setupWithNavController(navController,appBarConfiguration)
+
+        //Connect NavigationView with NavController
+        navigationView.setupWithNavController(navController)
     }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    //This part closes the drawer when it's open instead of closing the entire app on back press
+    override fun onBackPressed() {
+        /*
+        method 1 works whether dependencies have been added or not
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }*/
+        //method 2 works only with required dependencies. does the same thing as 1 with less code
+        if(drawerLayout.isOpen){
+            drawerLayout.close()
+        }else{
+            super.onBackPressed()
+        }
     }
 }
