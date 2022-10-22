b236d7a9a9b718c495854ec3119a1c15e4bc16d4
Kosisochukwu Eziaghighala <eziikay007@gmail.com>
2022-10-23 00:28:25 +0100

    Implementing a recyclerView for the main fragment

--- app/src/main/java/com/sriyank/globochat/city/CityListFragment.kt
+++ app/src/main/java/com/sriyank/globochat/city/CityListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sriyank.globochat.R
 
 
@@ -14,6 +16,28 @@ class CityListFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_city_list, container, false)
 
+        //create and call this method
+        setupRecyclerView(view)
+
         return view
+    }
+
+    private fun setupRecyclerView(view: View?) {
+        //get context of this fragment
+        val context = requireContext()
+        //create Adapter
+        val cityAdapter = CityAdapter(context, VacationSpots.cityList!!)
+        //reinitialize recyclerview
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.city_recycler_view)
+        //set Adapter
+        recyclerView?.adapter = cityAdapter
+        recyclerView?.setHasFixedSize(true)
+
+        //define layoutManager and set it to recyclerView
+        val layoutManager = LinearLayoutManager(context)
+        //this line below makes the items display vertically. It could be horizontal too... Getting ideas?
+        layoutManager.orientation = RecyclerView.VERTICAL
+        recyclerView?.layoutManager = layoutManager
+
     }
 }
